98819de329fdc77097c67be2aaa8937ebad38477
khun <36722210+khungry@users.noreply.github.com>
2023-10-07 22:56:21 -0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+// START NAVBAR JS
+
+function myFunction() {
+  var x = document.getElementById("mynavbar");
+  if (x.className === "navbar") {
+    x.className += " responsive";
+  } else {
+    x.className = "navbar";
+  }
+}
+
+// END NAVBAR JS
